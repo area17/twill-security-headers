600450c8c2f98e8c216dcef702552b8d48aeaa92
Antonio Ribeiro <antonio@area17.com>
2023-01-19 18:59:24 +0100

    Fix response

--- src/Services/Headers/RemoveUnwanted.php
+++ src/Services/Headers/RemoveUnwanted.php
@@ -3,10 +3,13 @@
 namespace A17\TwillSecurityHeaders\Services\Headers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 
 class RemoveUnwanted extends Header
 {
-    public function remove(Request $request): void
+    public function remove(Response|RedirectResponse|JsonResponse $response): void
     {
         if (!$this->securityHeaders->published) {
             return;
@@ -15,6 +18,6 @@ class RemoveUnwanted extends Header
         collect(explode(',', $this->securityHeaders->unwanted_headers))
             ->map(fn($header) => trim($header))
             ->filter()
-            ->each(fn($header) => $request->headers->remove($header));
+            ->each(fn($header) => $response->headers->remove($header));
     }
 }
